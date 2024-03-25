e994a4dc11bf95bd9ada1b005932f1da44ae7e4c
Khaled LAJILI <khaledlajili11@gmail.com>
2024-03-25 03:58:52 +0100

    resolve conflicts

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,29 @@
 import './styles/app.scss';
 
 import 'bootstrap'
+import 'slick-carousel'
+$(document).ready(function(){
+    $('.team-carousel').slick({
+        infinite: true,
+        slidesToShow: 1, // Number of slides to show at a time
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        prevArrow: '<button type="button" class="testimonial-prev d-none d-md-block btn btn-outline-dark rounded-circle p-1"><img src="build/images/icons/arrow_right.7282cdd7.svg" alt="arrow"/></button>',
+        nextArrow: '<button type="button" class="testimonial-next d-none d-md-block btn btn-outline-dark rounded-circle p-1"><img src="build/images/icons/arrow_left.cddca400.svg" alt="arrow"/></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
