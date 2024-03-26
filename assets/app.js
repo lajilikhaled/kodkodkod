5f02bec3989963ca1c92286adaba32bdeb7484d4
Khaled LAJILI <khaledlajili11@gmail.com>
2024-03-26 01:37:06 +0100

    team and map

--- assets/app.js
+++ assets/app.js
@@ -10,7 +10,7 @@ import './styles/app.scss';
 import 'bootstrap'
 import 'slick-carousel'
 $(document).ready(function(){
-    $('.team-carousel').slick({
+    $('.testimonial-carousel').slick({
         infinite: true,
         slidesToShow: 1, // Number of slides to show at a time
         slidesToScroll: 1,
@@ -18,6 +18,15 @@ $(document).ready(function(){
         arrows: true,
         prevArrow: '<button type="button" class="testimonial-prev d-none d-md-block btn btn-outline-dark rounded-circle p-1"><img src="build/images/icons/arrow_right.7282cdd7.svg" alt="arrow"/></button>',
         nextArrow: '<button type="button" class="testimonial-next d-none d-md-block btn btn-outline-dark rounded-circle p-1"><img src="build/images/icons/arrow_left.cddca400.svg" alt="arrow"/></button>',
+    });
+    $('.team-carousel').slick({
+        infinite: true,
+        slidesToShow: 4, // Number of slides to show at a time
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        prevArrow: '<button type="button" class="team-prev d-none d-md-block btn btn-outline-dark rounded-circle p-1"><img src="build/images/icons/arrow_right.7282cdd7.svg" alt="arrow"/></button>',
+        nextArrow: '<button type="button" class="team-next d-none d-md-block btn btn-outline-dark rounded-circle p-1"><img src="build/images/icons/arrow_left.cddca400.svg" alt="arrow"/></button>',
         responsive: [
             {
                 breakpoint: 768,
